9d09b0c86dac086c0ab7842b54617abad0084df1
zyan <132612230+zyanjyno@users.noreply.github.com>
2023-05-26 13:18:01 -0500

    se agrega

--- 1.13.2/src/pkg1/pkg13/pkg2/Main.java
+++ 1.13.2/src/pkg1/pkg13/pkg2/Main.java
@@ -4,18 +4,7 @@
  */
 package pkg1.pkg13.pkg2;
 import java.util.Scanner;
-/**
-En este ejemplo, utilizamos la clase Scanner para obtener el valor de n desde la entrada del usuario.
-* Luego, creamos un vector llamado numeros de tamaño n. 
-* Utilizamos un bucle for para recorrer el vector y asignar los números del 1 hasta n a cada elemento del vector.
-* Finalmente, imprimimos los números almacenados en el vector utilizando otro bucle for.
 
-Al ejecutar este programa, te pedirá ingresar el valor de n.
-* Después de ingresar el valor, mostrará los números del 1 hasta n almacenados en el vector. 
-* Por ejemplo, si ingresas 10, mostrará los números del 1 al 10.
-
- * @author ASUS
- */
 public class Main {
 
     /**
@@ -28,11 +17,11 @@ public class Main {
         System.out.println("Ingrese sus digitos aca");
    int n = scanner.nextInt();
     int[] numeros = new int[n];
-    for (int i = 0; i < numeros.length; i++) {
+    for (int i = 0; i < nu; i++) {
             numeros[i] = i + 1;
         }
         System.out.println("Los numeros almacenados son");
-        for(int i=0;i<numeros.length;i++){
+        for(int i=0;i<n;i++){
             System.out.println(numeros[i]);
         }
     }
