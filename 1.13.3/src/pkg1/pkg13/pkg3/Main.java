a8347a2b340b823fcecbe5bf0b97b7613f083070
zyan <132612230+zyanjyno@users.noreply.github.com>
2023-05-26 13:19:47 -0500

    se agrega

--- 1.13.3/src/pkg1/pkg13/pkg3/Main.java
+++ 1.13.3/src/pkg1/pkg13/pkg3/Main.java
@@ -4,26 +4,10 @@
  */
 package pkg1.pkg13.pkg3;
 import java.util.Scanner;
-/**
- *
- * @author ASUS
- */
+
 public class Main {
 
-    /**
-     En este trabajo, también utilizamos la clase Scanner para obtener el valor de n desde la entrada del usuario.
-     * Luego, creamos un vector llamado numeros de tamaño n.
-     * Utilizamos un bucle for para recorrer el vector y asignar los números del 1 hasta n a cada elemento del vector.
-
-Después de almacenar los números en el vector, utilizamos otro bucle for para imprimir el contenido del vector. 
-* En cada iteración, imprimimos el valor del elemento del vector seguido de un espacio en blanco.
-
-Al ejecutar este programa, te pedirá ingresar el valor de n.
-
-Después de ingresar el valor, mostrará el contenido del vector, 
-es decir, los números del 1 hasta n, separados por espacios. Por ejemplo, si ingresas 5, mostrará: 1 2 3 4 5.
-* 
-* 
+    
      * @param args the command line arguments
      */
     public static void main(String[] args) {
